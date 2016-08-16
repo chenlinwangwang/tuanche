83104567a807e2eb5d19d9b842cd839e2e30f2e3
chenlinwangwang <422828518@qq.com>
2016-08-16 16:56:53 +0800

    第二次

--- app/src/main/java/com/bwf/tuanche/MainActivity.java
+++ app/src/main/java/com/bwf/tuanche/MainActivity.java
@@ -22,39 +22,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv_test = (ListView) findViewById(R.id.lv_test);
-
-        List<String> list = new ArrayList<>();
-        list.add("123");
-        list.add("fdfd");
-        list.add("14143");
-        list.add("gsgd");
-        list.add("sdsd");
-        ReccleViewAdapter adapter = new ReccleViewAdapter(this);
-        adapter.settList(list);
-        lv_test.setAdapter(adapter);
-
-        test();
     }
-
-    public void test(){
-
-        String url = "http://119.254.70.199:8080/landz-app/house/houseBuySellList";
-
-        HttpHelper.getDetail(url,"0","10",new HttpCallBack<UserBean>() {
-
-            @Override
-            public void onFail(String errMsg) {
-
-            }
-
-            @Override
-            public void onSuccess(UserBean result) {
-
-
-            }
-
-        });
-    }
-
 }
